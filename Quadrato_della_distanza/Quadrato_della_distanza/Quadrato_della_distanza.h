f2ad798a1aa4943db42c59c514d77f738f9d72cb
almar <almar@PC_ALBY>
2025-10-19 18:31:48 +0200

    Refattorizzata funzione Qaudrato_Della_distanza
    È stata introdotta la struttura `quadrato` per rappresentare i punti
    con coordinate `x` e `y`. La funzione `Qaudrato_Della_distanza` è
    stata aggiornata per accettare oggetti `quadrato` come parametri
    invece di valori scalari. I test sono stati modificati per riflettere
    questo cambiamento e i valori attesi sono stati corretti.

--- Quadrato_della_distanza/Quadrato_della_distanza/Quadrato_della_distanza.h
+++ Quadrato_della_distanza/Quadrato_della_distanza/Quadrato_della_distanza.h
@@ -1,9 +1,14 @@
 #pragma once
+struct quadrato
+{
+	double x = 0;
+	double y = 0;
+};
 
-inline double Qaudrato_Della_distanza(double x1, double y1, double x2, double y2) {
-	double dx = x1-x2;
+inline double Qaudrato_Della_distanza(quadrato q1,quadrato q2) {
+	double dx = q1.x-q2.x;
 	dx = dx * dx;
-	double dy = y1-y2;
+	double dy = q1.y-q2.y;
 	dy = dy * dy;
 	return dx + dy;
 }
--- Quadrato_della_distanza/Quadrato_della_distanzaTest/test.cpp
+++ Quadrato_della_distanza/Quadrato_della_distanzaTest/test.cpp
@@ -3,9 +3,9 @@
 namespace Test {
 
 	TEST(Quadrato_della_distanzaTest, Quadrato_della_distanzaTestTrue) {
-		ASSERT_EQ(Qaudrato_Della_distanza(2, 4, 3, 5), 2);
+		ASSERT_EQ(Qaudrato_Della_distanza({2,3}, {4,5}), 8);
 	}
 	TEST(Quadrato_della_distanzaTest, Quadrato_della_distanzaTestTrue2) {
-		ASSERT_EQ(Qaudrato_Della_distanza(1, 2, 4, 6), 25);
+		ASSERT_EQ(Qaudrato_Della_distanza({1,4}, {2,6}), 5);
 	}
 }
